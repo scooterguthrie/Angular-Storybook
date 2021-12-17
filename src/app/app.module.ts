3ff689d97698d5aa300ebd78a407cf653ac5ef66
Scott Guthrie <jsguthrie@twc.com>
2021-12-17 17:16:47 -0500

    basic button samples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './components/button/button.component';
+import { ButtonComponent } from './components/_master-components/buttons/button.component';
+import { ButtonExamplesComponent } from './components/button-examples/button-examples.component';
+import { ButtonBaseComponent } from './components/button-examples/button-base/button-base.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    ButtonComponent,
+    ButtonExamplesComponent,
+    ButtonBaseComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/button-examples/button-base/button-base.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-base',
+  templateUrl: './button-base.component.html',
+  styleUrls: ['./button-base.component.scss']
+})
+export class ButtonBaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
