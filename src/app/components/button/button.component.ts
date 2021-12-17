53dd3bbb7fa153753a10770dac0af0941b2fbcc3
Scott Guthrie <jsguthrie@twc.com>
2021-12-17 10:00:13 -0500

    simple button component

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,13 +1,26 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-button',
+  selector: '[app-button]',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  styleUrls: ['./button.component.scss'],
+  host: {
+    'class': 'btn',
+    '[type]': 'buttonType',
+    '[class.btn-primary]': 'rank === "primary"',
+    '[class.btn-secondary]':'rank === "secondary"',
+    '[class.btn-link]': 'rank === "link"',
+    '[class.text-nowrap]': 'textNoWrap',
+    '[class.btn-sm]': 'size === "small"',
+    '[class.btn-lg]': 'size === "large"',
+    '[class.btn-block]': 'buttonBlock',
+    '[disabled]': 'buttonDisabled',
+    '[class.disabled]': 'buttonDisabled',
+    '[class.btn-sm-block]': 'buttonResponsive'
+  }
 })
 export class ButtonComponent {
   @Input() buttonLabel!: string;
-  @Input() addClass!: string;
   @Input() buttonType: "button" | "submit" | "reset" = "button";
   @Input() rank: "primary" | "secondary" | "link" = "primary";
   @Input() textNoWrap = false;
